17b17041abc52cc3c96915f15ae6008de5de9f11
Justin Kimbrell <justin@objectivehtml.com>
2014-12-13 20:04:30 -0500

    - (API) Added the NotificationTypeInterface

---
+++ core/interfaces/NotificationTypeInterface.php
@@ -0,0 +1,26 @@
+<?php
+namespace Craft\Plugins\Postmaster\Interfaces;
+
+use Craft\Postmaster_NotificationModel;
+use Craft\Postmaster_TransportModel;
+use Craft\Postmaster_TransportResponseModel;
+
+interface NotificationTypeInterface {
+	
+	public function init();
+
+	public function parse();
+
+	public function onBeforeSend(Postmaster_TransportModel $model);
+
+	public function onAfterSend(Postmaster_TransportResponseModel $model);
+
+	public function getInputHtml(Array $data = array());
+
+	public function getSettingsInputHtml(Array $data = array());
+
+	public function setNotificationModel(Postmaster_NotificationModel $notification);
+
+	public function getNotificationModel();
+
+}
